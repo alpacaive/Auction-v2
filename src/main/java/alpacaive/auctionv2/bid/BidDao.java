b5dc113ecd8d5eafdebd7c03b7e1ea5580e157af
ehfjd <ehfjd@hp_kk>
2024-07-11 17:28:59 +0900

    bid dao lock 추가

--- src/main/java/alpacaive/auctionv2/bid/BidDao.java
+++ src/main/java/alpacaive/auctionv2/bid/BidDao.java
@@ -3,12 +3,14 @@ package alpacaive.auctionv2.bid;
 import java.util.ArrayList;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import alpacaive.auctionv2.auction.Auction;
 import alpacaive.auctionv2.member.Member;
+import jakarta.persistence.LockModeType;
 
 @Repository
 public interface BidDao extends JpaRepository<Bid, Integer> {
@@ -17,6 +19,7 @@ public interface BidDao extends JpaRepository<Bid, Integer> {
 	ArrayList<Bid>findByParentOrderByNumDesc(Auction parent);
 	@Query("select b from Bid b where b.parent.num = :parent order by b.price desc")
 	ArrayList<Bid> findByBuyerByPrice(int parent);
+	@Lock(LockModeType.PESSIMISTIC_READ)
 	@Query("SELECT b FROM Bid b WHERE b.price = (SELECT MAX(b2.price) FROM Bid b2 WHERE b2.parent.num = :parent)")
 	Bid findMaxValue(@Param("parent") int parent);
 	
